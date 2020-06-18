7f88f80c5fe63983729899e1b2d6bb83e4145435
sunwen <sunwen586@163.com>
2020-06-18 17:11:22 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Index from './pages/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Index></Index>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+
+function Index() {
+  let a = 9;
+  return (
+    <div className="App">
+      {a}
+    </div>
+  );
+}
+
+export default Index;
